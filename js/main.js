58ac57880c4e9d3d97e35ea44429c9a93d922bd2
darqus <voladores@inbox.ru>
2022-06-25 03:56:37 +0300

    fix resizeHandler

--- js/main.js
+++ js/main.js
@@ -8,23 +8,30 @@ checkbox?.addEventListener('change', onChange)
 
 
 // add CSS property "overflow-y: scroll;" to header and footer if main is scrollable horizontally
-const header = document.getElementsByTagName('header')[0]
-const main = document.getElementsByTagName('main')[0]
-const footer = document.getElementsByTagName('footer')[0]
+const htmlTag = document.getElementsByTagName('html')[0]
+const headerTag = document.getElementsByTagName('header')[0]
+const mainTag = document.getElementsByTagName('main')[0]
+const footerTag = document.getElementsByTagName('footer')[0]
 
 const isScrollableNode = (e) => {
   return (e.offsetHeight < e.scrollHeight)
 }
 
 const resizeHandler = () => {
-  const isScrollableMain = isScrollableNode(main)
+  const isScrollableMain = isScrollableNode(mainTag)
 
   if (isScrollableMain) {
-    header.style.overflowY = 'scroll'
-    footer.style.overflowY = 'scroll'
+    htmlTag.style.overflowY = 'auto'
+    htmlTag.style.scrollbarWidth = 'initial'
+    htmlTag.style.scrollbarColor = 'initial'
+    headerTag.style.overflowY = 'scroll'
+    footerTag.style.overflowY = 'scroll'
   } else {
-    header.style.overflowY = 'initial'
-    footer.style.overflowY = 'initial'
+    htmlTag.style.overflowY = 'initial'
+    htmlTag.style.scrollbarWidth = 'thin'
+    htmlTag.style.scrollbarColor = '#7777c0 #e0e0e0'
+    headerTag.style.overflowY = 'initial'
+    footerTag.style.overflowY = 'initial'
   }
 }
 
